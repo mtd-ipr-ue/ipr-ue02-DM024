b3f359dc89c0fe0fbb1f717484ee8e2a32962302
Dorottya M. <s2410238088@fhooe.at>
2024-10-28 15:59:05 +0100

    ue1 und 2 gestartet

--- src/ue02_1.java
+++ src/ue02_1.java
@@ -2,6 +2,15 @@ import libraries.Out;
 
 public class ue02_1 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+            int num1 = 345;
+            int num2 = 5;
+            int num3 = 876;
+        
+           if (num1 > num2 && num1 > num3);  {
+              Out.println(num1);
+           } else if (num2 > num1 && num2 > num3); {
+              Out.println(num2);
+           } else Out.println(num3);
+         
     }
 }
--- src/ue02_2.java
+++ src/ue02_2.java
@@ -2,6 +2,23 @@ import libraries.Out;
 
 public class ue02_2 {
     public static void main(String[] args) {
-        Out.println("Hello, World!");
+        //int year = 2000;
+        int month = (1-12);
+        int days;
+        
+        switch(month) {
+            case 1, 3, 5, 7, 9, 10, 12:
+            days = 31;
+                break;
+            case 2:
+            days = 28;
+                break;
+            case 4, 6, 8, 11:
+            days = 30;
+                break;
+            default: Out.print("Error");
+
+
+        }
     }
 }
